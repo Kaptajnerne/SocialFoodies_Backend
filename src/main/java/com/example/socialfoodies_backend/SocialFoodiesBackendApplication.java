ea01fc04403c004015d45b020935504cc031d7d9
kjartanleander <Kjar0321@stud.kea.dk>
2023-11-20 13:20:37 +0100

    Initial push

---
+++ src/main/java/com/example/socialfoodies_backend/SocialFoodiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialfoodies_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialFoodiesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialFoodiesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/socialfoodies_backend/SocialFoodiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialfoodies_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFoodiesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
