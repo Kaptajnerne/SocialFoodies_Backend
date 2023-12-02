68643ac934519c353ffadd6b183b507e8446583a
Niclas Nielsen <niclas.nielsen@hotmail.com>
2023-12-02 15:19:33 +0100

    Finds and sets suggestion to customer ice cream with correct customer.

--- src/main/java/com/example/socialfoodies_backend/controller/CustomerIceCreamController.java
+++ src/main/java/com/example/socialfoodies_backend/controller/CustomerIceCreamController.java
@@ -1,7 +1,9 @@
 package com.example.socialfoodies_backend.controller;
 
+import com.example.socialfoodies_backend.model.Customer;
 import com.example.socialfoodies_backend.model.CustomerIceCream;
 import com.example.socialfoodies_backend.repository.CustomerIceCreamRepository;
+import com.example.socialfoodies_backend.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,24 +17,29 @@ import java.util.Optional;
 @CrossOrigin
 public class CustomerIceCreamController {
 
-        @Autowired
-        private CustomerIceCreamRepository customerIceCreamRepository;
+    @Autowired
+    CustomerIceCreamRepository customerIceCreamRepository;
 
-        @GetMapping()
-        public ResponseEntity<List<CustomerIceCream>> findAll() {
-            List<CustomerIceCream> customerIceCreams = customerIceCreamRepository.findAll();
-            return ResponseEntity.ok().body(customerIceCreams);
-        }
+    @Autowired
+    CustomerRepository customerRepository;
 
-        @GetMapping("/{id}")
-        public ResponseEntity<CustomerIceCream> findById(@PathVariable int id) {
-            Optional<CustomerIceCream> customerIceCream = customerIceCreamRepository.findById(id);
-            return customerIceCream.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-        }
-
-        @PostMapping()
-        public ResponseEntity<CustomerIceCream> create(@RequestBody CustomerIceCream customerIceCream) {
-            CustomerIceCream createdCustomerIceCream = customerIceCreamRepository.save(customerIceCream);
-            return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomerIceCream);
-        }
+    @GetMapping()
+    public ResponseEntity<List<CustomerIceCream>> findAll() {
+        List<CustomerIceCream> customerIceCreams = customerIceCreamRepository.findAll();
+        return ResponseEntity.ok().body(customerIceCreams);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerIceCream> findById(@PathVariable int id) {
+        Optional<CustomerIceCream> customerIceCream = customerIceCreamRepository.findById(id);
+        return customerIceCream.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("{customerID}")
+    public ResponseEntity<CustomerIceCream> create(@RequestBody CustomerIceCream customerIceCream, @PathVariable int customerID) {
+        Customer customer = customerRepository.findByCustomerID(customerID);
+        customerIceCream.setCustomer(customer);
+        CustomerIceCream createdCustomerIceCream = customerIceCreamRepository.save(customerIceCream);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomerIceCream);
+    }
+}
